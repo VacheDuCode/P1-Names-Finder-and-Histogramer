280af73254a37cbb12d4688b7bfc4dff7c5d243d
Matthew Naumann <138934377+VacheDuCode@users.noreply.github.com>
2025-10-31 18:16:29 -0500

    small changes

--- main.py
+++ main.py
@@ -8,6 +8,9 @@
 
 #The purpose of this file is to serve as a mini project to help learn python programming
 #The project is a name generator that generates random names based on user input that ...
+#P1 utilizes python ...
+
+
 
 
 def main():
