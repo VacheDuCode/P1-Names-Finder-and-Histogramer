963f7dfad36027f9f3df60a6f5ae0b7e191839ca
Matthew Naumann <138934377+VacheDuCode@users.noreply.github.com>
2025-10-31 16:42:50 -0500

    startup miniprojects

---
+++ main.py
@@ -0,0 +1,18 @@
+#import random
+#import numpy
+#import pandas
+#import math
+#import requests
+#import matplotlib
+#import openpyxl
+
+#The purpose of this file is to serve as a mini project to help learn python programming
+#The project is a name generator that generates random names based on user input that ...
+
+
+def main():
+    print('hello world')
+
+
+if __name__ == '__main__':
+    main()
