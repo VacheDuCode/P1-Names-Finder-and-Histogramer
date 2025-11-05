c65bfe21bfa96df71a1002c24ab23041d7f7e36e
Matthew Naumann <138934377+VacheDuCode@users.noreply.github.com>
2025-11-05 16:22:16 -0600

    making progress-still bugs, no histogram

---
+++ NameFinderCLASS.py
@@ -0,0 +1,241 @@
+#import random
+import numpy as np
+#import pandas
+#import math
+#import requests
+import matplotlib.pyplot as plt
+#import openpyxl
+import re
+# import itertools
+
+class NameFinder:
+
+#dont forget reuqirements.txt later!!!!......
+#redocleaning - delete lines with characters not letters or spaces or.....
+
+
+    def __init__(self) -> None:
+        print("Welcome to the Name Finder! \n")
+        self.get_names_file_data()
+        
+    def get_names_file_data(self) -> None:
+        self.get_names_file_from_user()
+        self.get_cleaned_lines_from_names_file()
+        self.check_names_file_is_empty()
+        self.get_names_from_file()
+
+
+    def get_names_file_from_user(self) -> None:
+        self.user_input = input('Please enter a text file to get names from (example: names.txt):')
+        
+        #debug
+        print(type(self.user_input), self.user_input)
+
+        self.try_getting_names_file()
+
+    def try_getting_names_file(self) -> None:
+        try:
+            self.names_file = open(self.user_input, 'r', encoding='utf-8')
+        except Exception as e:
+            print("Error: ", e)
+            self.get_names_file_from_user()
+        else:
+            self.names_file = open(self.user_input, 'r', encoding='utf-8')
+
+        
+    def check_names_file_is_empty(self) -> None:
+        #needs something here...
+        if self.cleaned_lines == '':  
+            print("Error: The file is empty. Please provide a non-empty file.")
+            self.names_file.close()
+            self.get_names_file_from_user()
+
+
+    def get_cleaned_lines_from_names_file(self) -> None:
+        # #debug
+        # linesss = []
+        # for line in self.names_file:
+        #     print('line2:', line)
+        #     linesss.append(line)
+        # print(linesss)
+        # #Why doesn't this list comprehension work?????????s
+        # linesss = [line for line in self.names_file]
+        # print(f"{linesss=}")
+        # print("get_cleaned:", linesss)
+
+        self.cleaned_lines = []
+        for line in self.names_file:
+            self.cleaned_lines.append(self.clean_line(line))
+        # #Why doesn't this list comprehension work?????????s
+        # self.cleaned_lines = [self.clean_line(line) for line in self.names_file]
+        if '' in self.cleaned_lines:
+            self.cleaned_lines.remove('')
+        
+        #debug
+        print(self.cleaned_lines)
+       
+        self.names_file.close()
+
+#redo this
+    def clean_line(self, line) -> str:
+        line = re.sub(r'[^a-zA-Z:\s-]', '', line)
+        line = line.strip()
+        line = line.title()
+        return line
+
+
+    def get_names_from_file(self) -> None:
+
+        ##debug
+        print(self.cleaned_lines)
+
+        self.check_for_correct_gender_headers()
+        self.check_for_names_under_both_gender_headers()
+        self.remove_trash_names()
+        self.remove_duplicate_names()
+        self.sort_names_by_first_letter()
+
+        self.boy_names = []
+        self.girl_names = []
+
+        for line in self.cleaned_lines:
+            if self.cleaned_lines.index(line) > self.cleaned_lines.index('Boy Names:') and self.cleaned_lines.index(line) < self.cleaned_lines.index('Girl Names:'):
+                self.boy_names.append(line)
+            elif self.cleaned_lines.index(line) > self.cleaned_lines.index('Girl Names:'):
+                self.girl_names.append(line)
+
+    def check_for_correct_gender_headers(self) -> None:
+        if 'Boy Names:' not in self.cleaned_lines or 'Girl Names:' not in self.cleaned_lines:
+            print("Error: The file must have header for 'Girl Names:' and 'Boy Names:'.")
+            self.names_file.close()          
+            self.get_names_file_from_user()
+        elif self.cleaned_lines.count('Boy Names:') > 1 or self.cleaned_lines.count('Girl Names:') > 1:
+            print("Error: The file must have only one 'Boy Names:' header and only one 'Girl Names:' header.")
+            self.names_file.close()
+            self.get_names_file_from_user()
+        elif self.cleaned_lines.index('Girl Names:') < self.cleaned_lines.index('Boy Names:'):
+            print("Error: The 'Boy Names:' header must come before the 'Girl Names:' header.")
+            self.names_file.close()
+            self.get_names_file_from_user()
+
+    def check_for_names_under_both_gender_headers(self) -> None:
+        if self.cleaned_lines[self.cleaned_lines.index('Boy Names:')+1] == 'Girl Names:' or self.cleaned_lines[self.cleaned_lines.index('Girl Names:')+1] == '':
+            print("Error: There must be at least one name under each gender header.")
+            self.names_file.close()
+            self.get_names_file_from_user()
+
+    def remove_trash_names(self) -> None:
+        for line in self.cleaned_lines:
+            if line != 'Boy Names:' and line != 'Girl Names:':
+                self.cleaned_lines.remove(line)
+            elif '  ' in line:
+                self.cleaned_lines.remove(line)
+            elif len(line) < 2:
+                self.cleaned_lines.remove(line) 
+            elif ":" in line and line != 'Boy Names:' and line != 'Girl Names:':
+                self.cleaned_lines.remove(line)
+
+    def remove_duplicate_names(self) -> None:
+        for line in self.cleaned_lines:
+            if self.cleaned_lines.count(line) > 1:
+                self.cleaned_lines.remove(line)
+
+    def sort_names_by_first_letter(self) -> None:
+        self.boy_names = {first_letter: name for name in  self.boy_names for first_letter in name[0]}
+        self.girl_names = {first_letter: name for name in  self.girld_names for first_letter in name[0]}
+
+
+    def make_cleaned_names_file(self) -> None:
+        with open('CleanedListOfNames.txt', 'w', encoding='utf-8') as cleaned_list_of_names_file:
+            for line in self.cleaned_lines:
+                cleaned_list_of_names_file.write(line + '\n')
+
+
+    def get_user_gender_preference(self) -> None:
+        self.gender_preference = input("Do you want a boy name or girl name? Enter b/g: ").lower() 
+        self.check_user_gender_preference_is_valid()
+
+    def check_user_gender_preference_is_valid(self) -> None:
+        if self.gender_preference not in ['b', 'g']:
+            print("Error: Please enter 'b' for a boy name or 'g' for a girl name.")
+            self.get_user_gender_preference()
+
+
+    def get_user_name_first_letter(self) -> None:
+        self.name_first_letter = input("Please enter the first letter of the name you want: ").lower()
+        self.check_user_name_first_letter_is_valid()
+    
+    def check_user_name_first_letter_is_valid(self) -> None:
+        if len(self.name_first_letter) != 1 or not self.name_first_letter.isalpha():
+            print("Error: Please enter a single letter (A-Z):")
+            self.get_user_name_first_letter()
+
+
+    def show_matching_names(self) -> None:
+        if self.gender_preference == 'b':
+            print(f"Here are the boy names starting with '{self.check_user_name_first_letter_is_valid}':\n")
+            print(self.boy_names[self.name_first_letter])
+
+        elif self.gender_preference == 'g':
+            print(f"Here are the girl names starting with '{self.check_user_name_first_letter_is_valid}':\n")   
+            print(self.girl_names[self.name_first_letter])
+
+
+    def ask_user_to_plot_histogram(self) -> None:
+        self.plot_histogram_preference = input("Do you want to see a histogram of the first letters of the names? (y/n): ").lower()
+        self.check_plot_histogram_preference_is_valid()
+
+        if self.plot_histogram_preference == 'y':
+            self.plot_name_first_letter_histogram()
+        elif self.plot_histogram_preference == 'n':
+            self.closing_message()
+
+    def check_plot_histogram_preference_is_valid(self) -> None:
+        if self.plot_histogram_preference not in ['y', 'n']:
+            print("Error: Please enter 'y' for yes or 'n' for no.")
+            self.ask_user_to_plot_histogram()
+
+    def plot_name_first_letter_histogram(self) -> None:
+        # Need 2 sub plots, one for each gender with bins for each letter of alphabet
+        # Need number of names that start with each letter above each bin
+        # Need arrow with chosen name above corresponding bin
+        print("Plotting histogram... (functionality not yet implemented)")
+
+        x = np.arange(0, 5, 0.1)
+        y = np.sin(x)
+
+        fig = plt.figure()
+        ax1_boy_names = fig.add_subplot(1, 2, 1)
+        ax2_girl_names = fig.add_subplot(1, 2, 2)
+        
+
+        ax1_boy_names.plot(x, y)
+
+        ax2_girl_names.plot(x, y)
+
+
+
+
+
+        plt.show()
+        self.closing_message()
+
+
+    def closing_message(self) -> None:
+        print("Thank you for using the Name Finder! Goodbye.")
+        exit()
+
+def main():
+    name_finder = NameFinder()
+
+    name_finder.make_cleaned_names_file()
+
+    name_finder.get_user_gender_preference()
+    name_finder.get_user_name_first_letter()
+
+    name_finder.show_matching_names()
+
+    name_finder.ask_user_to_plot_histogram()
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-This repo is a mini-project to practice to help learn python programming
-The project is a name generator that generates random names based on user input that ...
+This repo is a mini-project to practice to help learn python programming.
+The project is a name generator that generates random names from user inputted text file of names ...
+
+Uses: LongListOfNames.txt (messy, formatted), main.py (functionality), creates CleanListOfNames.txt file.
+
+It finally asks if the user wants the names histogramed by first letter and shows it using matplotlib
+
+1. gets user input: boy or girl name? first letter? max length?
+2. reads a file - handle issues, exceptions
+3. gets and cleans names-discards non-names
+4. sorts (into dict?) the boy names from girl names
+5. makes list of applicable names
+6. write new file with all cleaned names &
+10. returns list of good names
+11. histogram with arrow with name
+
+Try to keep  the code clean! :: high level to low level functions, functions do one thing only, exception handling is one function (try thing(), catch error(), finally ONLY), affinitys together, variables close to use (local or nonlocal -> top of function/class). Simple > Complex > Complicated. Use PEP 8 as much as possible.
+
+key words:
+-check
+get 
+-clean
+sort
+-show
